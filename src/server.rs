d3ea5177ce2d2b928a171515194c2318b233f449
Vladimír Zahradník <lada.zahradnik@gmail.com>
2023-06-20 16:30:43 +0200

    add basic proto structure

--- src/server.rs
+++ src/server.rs
@@ -1,38 +1,65 @@
 use tonic::{transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use link_eternal::{
+    link_service_server::{LinkService, LinkServiceServer},
+    AddLinkRequest, ListLinksRequest, ListLinksResponse,
+};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+pub mod link_eternal {
+    tonic::include_proto!("api");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct EternalLinkService {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
+impl LinkService for EternalLinkService {
+    async fn list_links(
         &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
+        request: Request<ListLinksRequest>,
+    ) -> Result<Response<ListLinksResponse>, Status> {
         println!("Got a request: {:?}", request);
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
+        let reply = link_eternal::ListLinksResponse {
+            data: vec![link_eternal::Link {
+                id: "1".to_string(),
+                url: "https://google.com".into(),
+                tags: vec!["search".into(), "google".into()],
+            }],
         };
 
         Ok(Response::new(reply))
     }
+
+    async fn add_link(
+        &self,
+        request: Request<AddLinkRequest>,
+    ) -> Result<Response<link_eternal::Link>, Status> {
+        println!("Got a request: {:?}", request);
+
+        let reply = link_eternal::Link {
+            id: "1".to_string(),
+            url: "https://google.com".into(),
+            tags: vec!["search".into(), "google".into()],
+        };
+
+        Ok(Response::new(reply))
+    }
+    // async fn list_links(
+    //     &self,
+    //     request: Request<ListLinksRequest>,
+    // ) -> Result<Response<super::ListLinksResponse>, Status>{
+    // Ok()
+    // };
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let greeter = EternalLinkService::default();
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(LinkServiceServer::new(greeter))
         .serve(addr)
         .await?;
 
